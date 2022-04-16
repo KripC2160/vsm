c21153294fd555fa1c8e964c69643eacd03cecba
KripC2160 <65854891+KripC2160@users.noreply.github.com>
2022-04-16 23:41:18 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 
 import bpy 
 import os 
+import requests
+import json
 
 from bpy.types import Operator 
 from bpy.props import (EnumProperty)
@@ -15,17 +17,34 @@ class vms_search_local(Operator):
     bl_label = "Search Skins"
     bl_property = "loc_search"
     
-    folder = bpy.path.abspath("C:\\Users\\user\\Desktop\\valimptestfolder")
-    blends = [f for f in os.listdir(folder) if f.endswith(".blend")]
-    
+    skin_codenames = []
+    skin_realnames = []
     items = ()
+    gun_names = ['Ares', 'Bucky', 'Bulldog', 'Classic', 'Frenzy', 'Ghost', 'Guardian', 'Judge', 'Marshal', 'Odin',
+                 'Operator', 'Phantom', 'Sheriff', 'Shorty', 'Spectre', 'Stinger', 'Vandal', 'Knife'
+    ]
+                
+    json_file = "https://raw.githubusercontent.com/KripC2160/vsm/main/weapons.json" #retreives the json file from the github repo
+    r = requests.get(json_file)
     
-    for blend in blends:
-        blend_path = os.path.join(folder, blend)
-        with bpy.data.libraries.load(blend_path) as (data_from, _):
-            for c in data_from.collections:
-                collitem = (c, c, "")
-                items = (collitem,) + items
+    with open("C:\\Users\\user\\Desktop\\weapons.json", 'wb') as f:
+        f.write(r.content)
+
+    input_content = open('C:\\Users\\user\\Desktop\\weapons.json')#r.content
+    json_array = json.load(input_content)
+    
+    for item in json_array:
+        skin_codenames.append(item['codename'])
+        skin_realnames.append(item['name'])
+    
+    for i in skin_realnames:
+        for gun in gun_names:
+            jsonitem = (i+' '+gun, i+' '+gun, "")
+            items = (jsonitem,) + items #adds the item in the tuple to be used for the search list
+
+
+    
+    #print(items)
     
     loc_search: EnumProperty(
         name="Local Search",
@@ -33,6 +52,7 @@ class vms_search_local(Operator):
     )
     
     def execute(self, context):
+        self.report({'INFO'}, "Selected:" + self.loc_search)
         return {"FINISHED"}
     
     def invoke(self, context, event):
